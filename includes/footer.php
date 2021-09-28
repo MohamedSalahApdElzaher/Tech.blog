f6cc6f85979580cbc6318d8ef399f0a927309212
Muhammad Salah <47748059+MohamedSalahApdElzaher@users.noreply.github.com>
2021-09-28 21:26:24 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,30 @@
   <!-- Footer -->
         <footer>
             <div class="row">
-                <div class="col-lg-12">
-                    <p style="text-align: center;">Made with &hearts; by 
-                        <a style="color:blue; font-weight:bold" href="https://github.com/MohamedSalahApdElzaher">MuhammadSalah</a> &copy; 2021</p>
+            
+             <div class="col-lg-12">
+                    <p style="text-align: center; font-size:18px; font-weight:bold;">Follow Me</p>
                 </div>
+      
+                <div class="col-lg-12">
+                         <p style="text-align: center;">
+                        <a style="color:blue;" href="https://www.linkedin.com/in/muhammad-sala7">LinkedIn</a></p>
+                            <p style="text-align: center;">
+                        <a style="color:blue;" href="https://github.com/MohamedSalahApdElzaher">Github</a></p>
+                         <p style="text-align: center;">
+                        <a style="color:blue;" href="https://www.facebook.com/profile.php?id=100004219846601">Facebook</a></p>
+                </div>
+                
+                 <div class="col-lg-12">
+                    <p style="text-align: center;">Made with &hearts; by 
+                        <a style="color:black; font-weight:bold" href="#">MuhammadSalah</a> &copy; 2021</p>
+                </div>
+                
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
         </footer>
+
     </div>
     <!-- /.container -->
 
